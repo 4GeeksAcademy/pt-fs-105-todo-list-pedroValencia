894095fff9739191ea4554c55b0a54cbb7966872
Pedro Valencia Alves <pedrovalenciaalves98@gmail.com>
2025-05-26 18:46:07 +0000

    proyecto finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,61 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [texto, setTexto] = useState("")
+	const [tareas, setTareas] = useState([])
+	
+	const manejarTexto = (event) => {
+		setTexto(event.target.value)
+	}
+	const manejarTareas = () => {
+		setTareas([...tareas, texto])
+		setTexto("")
+	}
+	const eliminarTareas = (tareaEliminar) => {
+		setTareas(tareas.filter((tarea, index) => index !== tareaEliminar)) 
+	}
+	const teclaEnter = (e) => {
+		if(e.key === "Enter") {
+			setTareas([...tareas, texto])
+			setTexto("")
+		}
+	}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+	return (
+		<>
+		<div style={{margin:10}}>
+		<input 
+		value = {texto}	
+		onChange={manejarTexto}
+		onKeyDown={teclaEnter}
+		/>
+		<button style={{padding: "4px 20px"}} type="button" className="btn btn-warning btn-sm" onClick={manejarTareas}>Enviar</button>
 		</div>
+		<div>
+		{tareas.length === 0 && (
+          <p style={{ margin: 10 }}>No hay tareas</p>
+        )}
+		<ul>
+			{tareas.map((tarea, index) => (
+				<li 
+				style={{backgroundColor:"#58d68d"}} 
+				key={index}>{tarea}
+				<button onClick={()=> eliminarTareas(index)} 
+				type="button" 
+				className="btn btn-outline-dark btn-sm"
+				style= {{marginLeft:20}}>✕</button>
+				</li>
+			)
+
+
+			)}
+		</ul>
+
+		</div>
+		</>
 	);
 };
 
